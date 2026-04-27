e4cf3f2edee02f94763623549e11d19adfeae3be
pristinet <32312877+pristinet@users.noreply.github.com>
2026-04-27 15:28:35 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
-  },
-}));
+export default defineConfig({
+  // This line is the secret sauce for GitHub Pages!
+  base: '/Your dream App/', 
+  plugins: [react()],
+})

6867587ec2bfd92159661b9f7dd0ca358a8144de
pristinet <32312877+pristinet@users.noreply.github.com>
2026-04-27 15:35:58 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   // This line is the secret sauce for GitHub Pages!
-  base: '/Your-dream-App/', 
+  base: '/Your-dream-app/', 
   plugins: [react()],
 })
